3187eae6c539dbd4eb7455cdf9fa9438c46244c7
Paul <paulhaeussler@gmx.de>
2022-06-21 18:43:54 +0200

    fix

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ class Website:
             self.__log(request)
             pass
 
-
-        self.app.run(host='0.0.0.0', port=5000)
+        if __name__ == '__main__':
+            self.app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     w = Website()
--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ from main import Website
 
 def build(dbuser, dbpw, dbhost, dbschema):
     w = Website(dbuser, dbpw, dbhost, dbschema)
+    w.create_app()
     return w.app
 
 
