593c20056218e2124c38d907eaef9b19b6207ccf
Paul <paulhaeussler@gmx.de>
2022-06-23 00:33:28 +0200

    got webgl working

--- static/gl.js
+++ static/gl.js
@@ -81,12 +81,12 @@ async function main(gl, name) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   var positions = [
-    0, 0,
-    0, 1,
-    1, 0,
-    0, 1,
-    1, 0,
-    1, 1
+    -1, 1,
+    -1, -1,
+    1, -1,
+    1, 1,
+    1, -1,
+    -1, 1
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
